f84510c77b2bcce9c705593c3f85d52ff94ce0c4
mangguung <guungofficial@gmail.com>
2020-06-09 21:03:59 +0700

    API URL Changes

--- lib/src/utils/util_constants.dart
+++ lib/src/utils/util_constants.dart
@@ -2,9 +2,9 @@ const String _APP_REST_URL_LOCAL1 = "http://192.168.43.116/siflab/inputapi/";
 
 const String _APP_REST_URL_LOCAL2 = "http://192.168.1.5/sipadang/inputapi/";
 
-const String _APP_REST_URL_HOSTD = "https://k3pawd.site/nim/inputapi/";
+const String _APP_REST_URL_HOSTD = "https://sipadang.k3pawd.site/inputapi/";
 
 const int APP_LEVEL_DOSEN = 5;
 const int APP_LEVEL_MAHASISWA = 4;
 
-const String APP_REST_URL = _APP_REST_URL_LOCAL2;
+const String APP_REST_URL = _APP_REST_URL_HOSTD;
