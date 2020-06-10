86ea166d4a1340e1b39c19eade39030e0f4d60f2
mangguung <guungofficial@gmail.com>
2020-06-10 12:20:34 +0700

    change url to http biasa

--- lib/src/utils/util_constants.dart
+++ lib/src/utils/util_constants.dart
@@ -2,7 +2,7 @@ const String _APP_REST_URL_LOCAL1 = "http://192.168.43.116/siflab/inputapi/";
 
 const String _APP_REST_URL_LOCAL2 = "http://192.168.1.5/sipadang/inputapi/";
 
-const String _APP_REST_URL_HOSTD = "https://sipadang.k3pawd.site/inputapi/";
+const String _APP_REST_URL_HOSTD = "http://sipadang.k3pawd.site/inputapi/";
 
 const int APP_LEVEL_DOSEN = 5;
 const int APP_LEVEL_MAHASISWA = 4;
